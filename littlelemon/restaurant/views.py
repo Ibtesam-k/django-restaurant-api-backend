b01ab3e3333ffb3c36f9ab802cc64fc143892921
ibtesam <ibtesam.kadourah@gmail.com>
2024-06-08 12:18:55 +0300

    create Menu api create menu APIView in views create restaurant urls file add menu api url include app urls in project urls file

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import BookingSerializer,MenuSerializer
+from .models import *
 # Create your views here.
+
+class MenuView(APIView):
+    def get(self, request):
+        items = Menu.objects.all()
+        serializer = MenuSerializer(items,many = True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer = MenuSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status":"success","data":serializer.data})
+        
