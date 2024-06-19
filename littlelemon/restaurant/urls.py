4be5a2bee05677a7cf3c504403c83301cdde164c
ibtesam <ibtesam.kadourah@gmail.com>
2024-06-19 19:31:20 +0300

    Testing Token authentication create a simple protected functional view try to access the view endpoint from Insomnia without and with Token (generated from admin panel)

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MenuItemsView, SingleMenuItemView
+from .views import MenuItemsView, SingleMenuItemView, msg
 urlpatterns = [
     #path('menu/',MenuView.as_view()),
     path('items/', MenuItemsView.as_view()),
     path('items/<int:pk>', SingleMenuItemView.as_view()),
+    path('message/', msg),
    
 ]
