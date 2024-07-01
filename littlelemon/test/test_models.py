cf754a1bc36bdc2b462d7cc6400615a37650fe63
ibtesam <ibtesam.kadourah@gmail.com>
2024-07-01 16:06:13 +0300

    add unit tests

---
+++ littlelemon/test/test_models.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from restaurant.models import Menu
+class MenuTest(TestCase):
+    def test_get_item(self):
+        item = Menu.objects.create(title="IceCream", price=80, inventory=100)
+        item_str = str(item)
+        self.assertEqual(item_str, "IceCream : 80")
---
+++ littlelemon/test/test_views.py
@@ -0,0 +1,26 @@
+from django.test import TestCase, Client
+from restaurant.models import Menu
+from django.contrib.auth.models import User
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from restaurant.serializers import MenuSerializer
+
+
+class MenuViewTest(TestCase):
+     def setUp(self):
+        self.model_instance = Menu.objects.create(title="Chocolate", price=70, inventory=78)
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.token = Token.objects.create(user=self.user)
+     def test_getall(self):
+         client = Client()
+         headers = {'HTTP_AUTHORIZATION': f'Token {self.token.key}'}
+
+         response = client.get('/restaurant/menu/',**headers)
+
+         self.assertEqual(response.status_code, 200)
+
+         serializer = MenuSerializer(self.model_instance)
+         self.assertEqual(response.data, [serializer.data])
+
+         
+        
