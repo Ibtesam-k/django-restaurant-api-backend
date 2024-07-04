060cfb64ce8ce04c82231df9e9b402cb0c2c5bd5
ibtesam <ibtesam.kadourah@gmail.com>
2024-07-04 10:27:26 +0300

    set databases variable to default

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -76,8 +76,20 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-from .local_settings import DATABASES_SETTINGS
-DATABASES = DATABASES_SETTINGS
+#from .local_settings import DATABASES_SETTINGS
+DATABASES = {
+ 'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'LittleLemon',  
+        'USER': 'yourusername',  
+        'PASSWORD': 'yourpassword',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    } 
+}
 
 
 # Password validation
